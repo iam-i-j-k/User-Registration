61f3abe71643d9cc63e3ecac40b72a65ad675bdc
Irfan <irfanjankhan7860@gmail.com>
2024-10-02 12:48:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('user-form').addEventListener('submit', function(e) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        e.preventDefault();
+    }
+});
